209a36543c4ee1553e20bb37f3df199a0e923561
hamid <zmn-hamid@proton.me>
2024-06-08 00:53:43 +0330

    handle remaining for wrong ocr results

--- app.py
+++ app.py
@@ -17,12 +17,23 @@ def time_exceeded():
     return datetime.now() >= END_AT
 
 
+def handled_remaining():
+    for _ in range(3):
+        remaining = int(find_remaining(ocr(take_screenshot(SCREENSHOT_PATH))))
+        if remaining == None:
+            time.sleep(1)
+            continue
+        else:
+            return remaining
+    else:
+        raise Exception("cant find remaining")
+
+
 # do tapping until finished
 time_before_operation = time.time()
 while not time_exceeded():
-    number_of_taps = int(
-        find_remaining(ocr(take_screenshot(SCREENSHOT_PATH))) / TAP_WEIGHT
-    )
+    for idx in range(3):
+        number_of_taps = int(handled_remaining() / TAP_WEIGHT)
     print(f"tapping at {datetime.now()}...")
     while True:
         # do tapping
@@ -35,7 +46,7 @@ while not time_exceeded():
                 time.sleep(random.uniform(8, 12))
 
         # check how much left
-        remaining = find_remaining(ocr(take_screenshot(SCREENSHOT_PATH)))
+        remaining = handled_remaining()
 
         # check if less than 5 exit
         if remaining < COOLDOWN_TAPS_LEFT:
--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 WHERE_TO_TAP = 542, 761  # X Y coordinate
 
-MAXIMUM_NUMBER_OF_TAPS = 4000
+MAXIMUM_NUMBER_OF_TAPS = 7000
 
-TAP_WEIGHT = 7  # how man taps does each tap have (4 means when you tap, it adds 4 taps)
+TAP_WEIGHT = 13  # how man taps does each tap have
 
-COOLDOWN_TIME = 400  # in seconds
+COOLDOWN_TIME = 60  # in seconds
 COOLDOWN_TAPS_LEFT = 20  # starts cooling down when this much is left
 
 MAXIMUM_TIME_RUNNING = 6 * 60 * 60  # in seconds
