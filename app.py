c96ca68271c6d83e267171d9d970588e30d0e677
hamid <zmn-hamid@proton.me>
2024-06-09 13:15:20 +0330

    just tap option + one file

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from main import do_a_set_of_tapping, take_screenshot, ocr, find_remaining
-from config import (
-    SCREENSHOT_PATH,
-    COOLDOWN_TIME,
-    MAXIMUM_TIME_RUNNING,
-    COOLDOWN_TAPS_LEFT,
-    TAP_WEIGHT,
-)
-import time, random
+import os
+import random
+import re
+import time
 from datetime import datetime, timedelta
+
+import click
+import pytesseract
+from PIL import Image
+
+from config import (
+    COOLDOWN_TAPS_LEFT,
+    COOLDOWN_TIME,
+    MAXIMUM_NUMBER_OF_TAPS,
+    MAXIMUM_TIME_RUNNING,
+    SCREENSHOT_PATH,
+    TAP_WEIGHT,
+    WHERE_TO_TAP,
+)
+from main import do_a_set_of_tapping, do_tapping, find_remaining, ocr, take_screenshot
 
 END_AT = datetime.now() + timedelta(seconds=MAXIMUM_TIME_RUNNING)
 MAX_CHUNK_SIZE = 500
@@ -17,46 +27,89 @@ def time_exceeded():
     return datetime.now() >= END_AT
 
 
+def do_tapping(tap_coordinate):
+    os.system(f"adb shell input tap {tap_coordinate[0]} {tap_coordinate[1]}")
+
+
+def do_a_set_of_tapping(number_of_taps: int):
+    for _ in range(int(number_of_taps)):
+        do_tapping(WHERE_TO_TAP)
+
+
+def take_screenshot(screenshot_file: str):
+    os.system(
+        f"adb shell screencap /sdcard/{screenshot_file} && adb pull /sdcard/{screenshot_file}"
+    )
+    return screenshot_file
+
+
+def ocr(image_path: str):
+    image = Image.open(image_path)
+    return pytesseract.image_to_string(image)
+
+
+def find_remaining(ocr_text: str):
+    """returns remaining and total"""
+    all_the_numbers = re.findall(r"\d+", ocr_text)[::-1]
+    for idx, number in enumerate(all_the_numbers):
+        if int(number) == MAXIMUM_NUMBER_OF_TAPS:
+            return int(all_the_numbers[idx + 1])
+
+
 def handled_remaining():
-    for _ in range(3):
-        remaining = int(find_remaining(ocr(take_screenshot(SCREENSHOT_PATH))))
+    for _ in range(5):
+        remaining = find_remaining(ocr(take_screenshot(SCREENSHOT_PATH)))
         if remaining == None:
-            time.sleep(1)
+            time.sleep(1.5)
             continue
         else:
-            return remaining
+            return int(remaining)
     else:
         raise Exception("cant find remaining")
 
 
-# do tapping until finished
-time_before_operation = time.time()
-while not time_exceeded():
-    for idx in range(3):
+def smart_tap():
+    while not time_exceeded():
         number_of_taps = int(handled_remaining() / TAP_WEIGHT)
-    print(f"tapping at {datetime.now()}...")
-    while True:
-        # do tapping
-        chunks = [MAX_CHUNK_SIZE] * (number_of_taps // MAX_CHUNK_SIZE) + (
-            [number_of_taps % MAX_CHUNK_SIZE] if number_of_taps % MAX_CHUNK_SIZE else []
-        )
-        for chunk in chunks:
-            do_a_set_of_tapping(chunk)
-            if len(chunks) > 1:
-                time.sleep(random.uniform(8, 12))
+        print(f"tapping at {datetime.now()}...")
+        while True:
+            # do tapping
+            chunks = [MAX_CHUNK_SIZE] * (number_of_taps // MAX_CHUNK_SIZE) + (
+                [number_of_taps % MAX_CHUNK_SIZE]
+                if number_of_taps % MAX_CHUNK_SIZE
+                else []
+            )
+            for chunk in chunks:
+                do_a_set_of_tapping(chunk)
+                if len(chunks) > 1:
+                    time.sleep(random.uniform(8, 12))
 
-        # check how much left
-        remaining = handled_remaining()
+            # check how much left
+            remaining = handled_remaining()
 
-        # check if less than 5 exit
-        if remaining < COOLDOWN_TAPS_LEFT:
+            # check if less than 5 exit
+            if remaining < COOLDOWN_TAPS_LEFT:
+                break
+
+            # calulate new tapping amount
+            number_of_taps = int(remaining / TAP_WEIGHT)
+        if time_exceeded():
             break
 
-        # calulate new tapping amount
-        number_of_taps = int(remaining / TAP_WEIGHT)
-    if time_exceeded():
-        break
+        # cooldown
+        print(f"resting at {datetime.now()}...")
+        time.sleep(COOLDOWN_TIME)
 
-    # cooldown
-    print(f"resting at {datetime.now()}...")
-    time.sleep(COOLDOWN_TIME)
+
+def just_tap():
+    while True:
+        do_tapping(WHERE_TO_TAP)
+
+
+print("1. smart tap\n2. just tap (continuously with no feature)\n")
+choice = click.prompt("choose", type=click.Choice(["1", "2"]))
+
+if choice == "1":
+    smart_tap()
+else:
+    just_tap()
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ MAXIMUM_NUMBER_OF_TAPS = 7000
 TAP_WEIGHT = 13  # how man taps does each tap have
 
 COOLDOWN_TIME = 60  # in seconds
-COOLDOWN_TAPS_LEFT = 20  # starts cooling down when this much is left
+COOLDOWN_TAPS_LEFT = 40  # starts cooling down when this much is left
 
 MAXIMUM_TIME_RUNNING = 6 * 60 * 60  # in seconds
 # maximum seconds of running the script (is ignored during the tapping or resting)
